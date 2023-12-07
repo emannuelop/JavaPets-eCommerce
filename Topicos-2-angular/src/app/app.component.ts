cf09e650061ff43207850a7ce24aa6d198c1274c
Matheus-Uchoa/ <matheusuchoa@unitins.br>
2023-12-07 20:19:39 -0300

    correção de rotas no onInit da aplicação

--- Topicos-2-angular/src/app/app.component.ts
+++ Topicos-2-angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { LocalStorageService } from './services/local-storage-service';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,18 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   title = 'angular-topicos2-2023-2';
   currentPage: string;
-  constructor(private router: Router, private authService: AuthService) {  this.currentPage = '';}
+  constructor(private router: Router, private authService: AuthService,  private localStorageService: LocalStorageService,) {  this.currentPage = '';}
   
   ngOnInit() {
     if (!this.authService.isTokenExpired()){
       this.currentPage = this.router.url;
      console.log(this.currentPage);
-     if(this.currentPage === "/"){
+     const usuarioLogado = this.localStorageService.getItem('usuario_logado');
+     if(this.currentPage === "/" && usuarioLogado.perfis.length === 1 ){
+     
       this.router.navigateByUrl("/user/home/home-page")
      }
+     console.log(this.router.url)
     }else{
       this.router.navigateByUrl('/auth/login');
     this.currentPage = this.router.url;
